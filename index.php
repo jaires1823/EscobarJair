7cbd56989ee66c6fc2db4bde7207851044c9f373
jairescobar <jaires1823@gmail.com>
2022-10-30 19:19:03 -0500

    cambie mi foto

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ include("marco-right.php");
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Stalin Francis<br>
-                Fecha: 2021-08-17 <br>
+                Posteado por:  Jair Escobar<br>
+                Fecha: 2022-10-30 <br>
           </div>
         </div>
     </div>
@@ -76,9 +76,9 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Stalin"; 
+ document.title="Jair"; 
 document.getElementById("foto").src="./images/stalin.png";
-document.getElementById("elnombre").innerHTML="Stalin Francis";
+document.getElementById("elnombre").innerHTML="Jair Escobar";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
